938706541fcb040414b8b42c08b399854fd158a2
zbs <90139766+zbs-test@users.noreply.github.com>
2023-01-10 13:28:37 +0800

    Create day02.cpp

---
+++ before/day02.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+#include <string>
+// chater2 literals 
+// sg1 this 
+//thit 不能用于static 方法
+class Test {
+public:
+	string str;
+	static string say_hello() {
+		//this->str = "hello world"; 不能这样做
+		return "hello world";
+	}
+};
+int main(){
+	// 我不知道shared_ptr 是什么,但是我想尝鲜
+	std::shared_ptr<Test> ptr = make_shared<Test>();
+	cout << ptr->say_hello()<< endl;
+}
